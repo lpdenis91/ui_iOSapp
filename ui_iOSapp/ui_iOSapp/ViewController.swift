762928d96ba32c8a6a44059ec371c400b74c2e01
Denis Burdin <lpdenis91@gmail.com>
2021-01-29 14:28:14 +0500

    init

---
+++ ui_iOSapp/ui_iOSapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ui_iOSapp
+//
+//  Created by u on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
